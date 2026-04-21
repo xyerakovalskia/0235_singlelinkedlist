c0b1d7e846d630610c6eb6f6a71a026a75d8ebb7
xyerakovalskia <xyera123987@gmail.com>
2026-04-21 11:50:24 +0700

    menambahkan bool delnode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -77,3 +77,19 @@ public:
 
     return (current != NULL);
 }
+
+bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else 
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
