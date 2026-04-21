51bc2021799340b4408d79d65acd73062481c0ec
xyerakovalskia <xyera123987@gmail.com>
2026-04-21 11:42:25 +0700

    menambahkan while di void addnode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -40,3 +40,21 @@ public:
             START = nodeBaru;
             return;
         }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
