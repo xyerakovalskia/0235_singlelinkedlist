c66aea53f8ec292f42ff2e23ffd37b34ea360570
xyerakovalskia <xyera123987@gmail.com>
2026-04-21 11:38:24 +0700

    menambahkan if di void addnode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -27,3 +27,16 @@ public:
 
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diizinkan\n";
+                return;
+            }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
